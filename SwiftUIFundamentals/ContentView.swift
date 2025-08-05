b2b44615cafb10f1401912cab8f6a12c87f90bc6
Ankith <ankith.kirti@nic.in>
2025-08-05 15:10:56 +0530

    add animation to text and image

--- SwiftUIFundamentals/ContentView.swift
+++ SwiftUIFundamentals/ContentView.swift
@@ -10,31 +10,53 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
+    @State private var imageNumber = 0
+    @State private var index = 0
+    
+    let messages = ["You are Awesome!",
+                    "You are Great!",
+                    "You are an Astronaut",
+                    "You are a champion!",
+                    "You are Truth seeker!",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    "Cras urna lectus, pellentesque at condimentum ut, mattis sit amet diam."]
     
     var body: some View {
         VStack {
-            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 120)
+                .animation(.easeInOut(duration: 0.15), value: imageName)
+                
             Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .clipShape(.rect(cornerRadius: 30))
+                .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-            
-            Text(message)
-                .font(.title)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
+                .animation(.default, value: imageName)
             
             Spacer()
-                        
+            
             Button("Press me!") {
-                let image1 = "tiger"
-                let image2 = "elephant"
-                let message1 = "You are Awesome!"
-                let message2 = "You are Great!"
                 
-                message = (message == message1 ? message2 : message1)
-                imageName = (imageName == image1 ? image2 : image1)
+                message = messages[index]
+                index+=1
+                
+                imageName = "image"+"\(imageNumber)"
+                imageNumber+=1
+                                
+                if index > messages.count-1{
+                    index = 0
+                }
+                
+                if imageNumber > 7{
+                    imageNumber = 0
+                }
+                
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
